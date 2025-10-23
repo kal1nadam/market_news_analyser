a7b4032c9736b47a7715d2f901c1713adae085c9
kal1nadam <kalinaadam75@seznam.cz>
2025-10-23 18:45:06 +0200

    DI registration

--- src/NewsAnalyzer.Infrastructure/DependencyInjection.cs
+++ src/NewsAnalyzer.Infrastructure/DependencyInjection.cs
@@ -2,8 +2,12 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NewsAnalyzer.Application.Common.Interfaces;
+using NewsAnalyzer.Infrastructure.Bus;
 using NewsAnalyzer.Infrastructure.Common;
+using NewsAnalyzer.Infrastructure.External.News;
+using NewsAnalyzer.Infrastructure.Outbox;
 using NewsAnalyzer.Infrastructure.Persistence;
+using NewsAnalyzer.Infrastructure.Workers;
 
 namespace NewsAnalyzer.Infrastructure;
 
@@ -12,12 +16,27 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddSingleton<IDateTimeProvider, SystemDateTimeProvider>();
+        services.AddSingleton<IEventBus, ChannelEventBus>();
+        
+        services.AddScoped<INewsProvider, NewsProvider>();
+        services.AddScoped<IOutboxStore, OutboxStore>();
+
+        // Background services
+        services.AddHostedService<OutboxPublisherService>();
+        
         
         // Configure DbContext with SQLite
         var connectionString = configuration.GetConnectionString("AppDb") ?? "Data Source=App_Data/NewsAnalyzer.db";
         services.AddDbContext<AppDbContext>(opts => opts.UseSqlite(connectionString));
         
         services.AddScoped<IAppDbContext>(provider => provider.GetRequiredService<AppDbContext>());
+        
+        
+        // Http Clients
+        services.AddHttpClient<NewsProvider>(client =>
+        {
+            client.BaseAddress = new Uri("https://financialmodelingprep.com/stable/");
+        });
 
         return services;
     }
