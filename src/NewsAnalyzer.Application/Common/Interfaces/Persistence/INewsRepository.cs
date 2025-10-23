3b08ed8d93845cabc349bf72a26f7c0dbce93be0
kal1nadam <kalinaadam75@seznam.cz>
2025-10-23 23:42:25 +0200

    News repository update

---
+++ src/NewsAnalyzer.Application/Common/Interfaces/Persistence/INewsRepository.cs
@@ -0,0 +1,10 @@
+namespace NewsAnalyzer.Application.Common.Interfaces.Persistence;
+
+/// <summary>
+/// Repository for managing news items.
+/// </summary>
+public interface INewsRepository
+{
+    Task<IReadOnlyList<Guid>> InsertAsync(IReadOnlyList<Entities.News> items, CancellationToken ct);
+    Task<IReadOnlyList<Entities.News>> GetAsync(int page, int pageSize, CancellationToken ct);
+}
--- src/NewsAnalyzer.Infrastructure/Persistence/NewsRepository.cs
+++ src/NewsAnalyzer.Infrastructure/Persistence/NewsRepository.cs
@@ -1,4 +1,7 @@
+using Microsoft.EntityFrameworkCore;
 using NewsAnalyzer.Application.Common.Interfaces;
+using NewsAnalyzer.Application.Common.Interfaces.Persistence;
+using NewsAnalyzer.Application.Entities;
 
 namespace NewsAnalyzer.Infrastructure.Persistence;
 
@@ -11,5 +14,19 @@ public sealed class NewsRepository : INewsRepository
         _dbContext = dbContext;
     }
     
-    // TODO retrieve news from the database
+    public async Task<IReadOnlyList<Guid>> InsertAsync(IReadOnlyList<News> items, CancellationToken ct)
+    {
+        await _dbContext.News.AddRangeAsync(items, ct);
+        return items.Select(i => i.Id).ToList();
+    }
+
+    public async Task<IReadOnlyList<News>> GetAsync(int page, int pageSize, CancellationToken ct)
+    {
+        return await _dbContext.News
+            .AsNoTracking()
+            .OrderByDescending(n => n.PublishedAt)
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync(ct);
+    }
 }
