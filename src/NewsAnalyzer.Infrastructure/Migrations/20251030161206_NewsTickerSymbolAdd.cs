ae6108dae078901b44acf1550b95d4d521a75fc5
kal1nadam <kalinaadam75@seznam.cz>
2025-10-30 17:12:32 +0100

    News ticker symbool add - db migrations

---
+++ src/NewsAnalyzer.Infrastructure/Migrations/20251030161206_NewsTickerSymbolAdd.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewsAnalyzer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewsTickerSymbolAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TickerSymbol",
+                table: "news",
+                type: "TEXT",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TickerSymbol",
+                table: "news");
+        }
+    }
+}
