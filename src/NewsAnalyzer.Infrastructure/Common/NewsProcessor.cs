5a6a0bb2467d848545c7f60809d5307ca7db62d2
kal1nadam <kalinaadam75@seznam.cz>
2025-10-30 14:35:26 +0100

    NewsProcessor update - call OpenAi to analyze - mark as analyzed

--- src/NewsAnalyzer.Infrastructure/Common/NewsProcessor.cs
+++ src/NewsAnalyzer.Infrastructure/Common/NewsProcessor.cs
@@ -1,5 +1,6 @@
 using NewsAnalyzer.Application.Common.Interfaces;
 using NewsAnalyzer.Application.Common.Interfaces.Persistence;
+using NewsAnalyzer.Application.DTO;
 
 namespace NewsAnalyzer.Infrastructure.Common;
 
@@ -8,25 +9,42 @@ public sealed class NewsProcessor : INewsProcessor
     private readonly INewsRepository _newsRepository;
     private readonly IUnitOfWork _unitOfWork;
     private readonly INewsNotifier _newsNotifier;
+    private readonly IAiAnalyzer _aiAnalyzer;
     
-    public NewsProcessor(INewsRepository newsRepository, IUnitOfWork unitOfWork, INewsNotifier newsNotifier)
+    public NewsProcessor(INewsRepository newsRepository, IUnitOfWork unitOfWork, INewsNotifier newsNotifier, IAiAnalyzer aiAnalyzer)
     {
         _newsRepository = newsRepository;
         _unitOfWork = unitOfWork;
         _newsNotifier = newsNotifier;
+        _aiAnalyzer = aiAnalyzer;
     }
     
     public async Task EnrichNewsAsync(Guid newsId, CancellationToken ct = default)
     {
         var news = await _newsRepository.GetByIdAsync(newsId, ct);
         
-        // TODO call openAI api to analyze
         if (news is not null)
         {
-            news.ImpactPercentage = new Random().Next(1, 101);
-            news.MarketTrend = "Up";
-            news.ReasonForMarketTrend = "Positive news impact";
+            EnrichNewsPromptDto param = new()
+            {
+                Headline = news.Headline,
+                Summary = news.Summary,
+                TickerSymbol = news.TickerSymbol,
+                Url = news.Url,
+            };
             
+            // Call AI analyzer - using OpenAI to analyze
+            var analysis =  await _aiAnalyzer.AnalyzeNewsAsync(param, ct);
+            
+            if (analysis is not null)
+            {
+                news.ImpactPercentage = (decimal)analysis.ImpactPercent / 100; // Convert into percentage
+                news.MarketTrend = analysis.MarketTrend;
+                news.ReasonForMarketTrend = analysis.ReasonForMarketTrend;
+            }
+            
+            // Update news as analyzed
+            news.MarkAnalyzed();
             await _unitOfWork.SaveChangesAsync(ct);
         }
         
