b73b1135c0f54baa1e08d340aac40c2c388de75b
kal1nadam <kalinaadam75@seznam.cz>
2025-11-01 23:52:52 +0100

    Background worker - logging add

--- src/NewsAnalyzer.Infrastructure/Workers/NewsAnalyzerHostedService.cs
+++ src/NewsAnalyzer.Infrastructure/Workers/NewsAnalyzerHostedService.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using NewsAnalyzer.Application.Common.Interfaces;
 using NewsAnalyzer.Application.Events;
 
@@ -9,12 +10,13 @@ public sealed class NewsAnalyzerHostedService : BackgroundService
 {
     private readonly IServiceProvider _serviceProvider;
     private readonly IEventBus _bus;
-    // TODO logging
+    private readonly ILogger _logger;
     
-    public NewsAnalyzerHostedService(IServiceProvider serviceProvider, IEventBus bus)
+    public NewsAnalyzerHostedService(IServiceProvider serviceProvider, IEventBus bus, ILogger<NewsAnalyzerHostedService> logger)
     {
         _serviceProvider = serviceProvider;
         _bus = bus;
+        _logger = logger;
     }
     
     protected override async Task ExecuteAsync(CancellationToken ct)
@@ -32,7 +34,7 @@ public sealed class NewsAnalyzerHostedService : BackgroundService
                 }
                 catch (Exception ex)
                 {
-                    // TODO log error
+                    _logger.LogError(ex, "Error processing NewsCreated event for NewsId: {NewsId}", evt.NewsId);
                 }
             }
         }
